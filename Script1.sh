d16e02f88906aa136b8a2b80681dac47b1bb7de0
atulv1994 <vaidyaatul13@gmail.com>
2022-11-12 07:54:40 +0530

    my first edit

--- Script1.sh
+++ Script1.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #This is my 1st shell script to print output
-echo "Hello All, Welcome to AWS/Devops Class"
-echo "Hello, How are you?"
-echo "Welcome to Young Minds"
-echo "Best class of AWS/DevOps"
-echo "We are learning git"
-echo "We are done with git"
+echo "Hello"
+echo "i am atul"
+echo "how are you?"
+
